70657c3820cbb182290364db2690cb702dcadc2a
vut190408-sketch <vut190408@gmail.com>
2026-08-05 09:43:23 +0700

    update script.js

--- script.js
+++ script.js
@@ -1036,7 +1036,7 @@ let QUIZ_DATA_SETS = {};
         // =========================================================================
         // === HỆ THỐNG KIỂM TRA PHIÊN BẢN TỰ ĐỘNG (AUTO-UPDATE) ===
         // =========================================================================
-        const CURRENT_VERSION = 1785897771584;
+        const CURRENT_VERSION = 1785897797583;
         const CHECK_INTERVAL = 3 * 60 * 1000;
 
         async function checkUpdate() {
